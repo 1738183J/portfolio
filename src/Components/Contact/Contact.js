f90ef01e5b77716d287be2a1da9c91abfe07ce56
1738183J <1738183@cstj.qc.ca>
2023-01-24 15:17:15 -0500

    Contact fonctionnel

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React,  { useRef }  from "react";
+import emailjs from 'emailjs-com';
 import "./Contact.scss";
 import {ReactComponent as MailLogo} from '../../Images/144-envelope-mail-notification.svg';
 import { Container, Row, Col } from "react-bootstrap";
@@ -8,7 +9,20 @@ import Linkedin from '../../Images/linkedin.png';
 import Facebook from '../../Images/facebook.png';
 
 const Contact = props => {
+    
+    const form = useRef();
 
+    function sendEmail(e){
+        console.log('test');
+        e.preventDefault();
+
+        emailjs.sendForm("service_s6b4sio", "template_v1m6wzx", e.target, "OndW3tj310zUff-v_")
+        .then(result => {
+            console.log(result.text);
+        }, (error)=> {
+            console.log(error.text);
+        });
+    }
     return (
         <div className="contact-container" id="contactContainer">
         <Container>
@@ -17,7 +31,7 @@ const Contact = props => {
                 <MailLogo className="wiggle"/>
             </Row>
             <Row className="form-contact">
-                <form className="contact_form w-100">
+                <form className="form" ref={form} onSubmit={sendEmail} >
                         <Col className="form-group">
                             <h3>Name</h3>
                             <input className="form-control" id="name" name="name" placeholder="Name" type="text"/>
@@ -26,7 +40,7 @@ const Contact = props => {
                             <h3>Message</h3>
                             <textarea className="form-control3" name="message" id ="message" placeholder="Message" rows ='5'></textarea>
                             <Col className="submit-form">
-                                <button className="submit-btn" type="submit">Submit</button>
+                                <button onSubmit = {sendEmail}  ref={form} className="submit-btn">Submit</button>
                             </Col>
                             <div className="media-btn">
                                 <a className="btn-m" href="https://www.linkedin.com/in/jacob-godbout-4853a6220/"><img href="https://www.linkedin.com/in/jacob-godbout-4853a6220/" src = {Linkedin} className="reseau-icon" alt="media"/></a>
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Grid.scss';
 import Ardoise from '../../Images/Ardoise.png';
 import Portfolio from '../../Images/Portfolio.png';
+import Oni from'../../Images/oni.png';
 
 const Grid = props => {
 
@@ -9,7 +10,7 @@ const Grid = props => {
         <div className="grid-container">
             <div className='item'><a href="https://adroise.netlify.app/"><img src = {Ardoise} className="ardoise" alt=""/></a></div>
             <div className='item'><a href="https://jacobgodbout3.myportfolio.com/"><img src = {Portfolio} className="portfolio" alt=""/></a></div>
-            <div className='item'><a href="https://jacobgodbout3.myportfolio.com/"><img src = {Portfolio} className="portfolio" alt=""/></a></div>
+            <div className='item'><a href="https://oniorigin.web.app/"><img src = {Oni} className="portfolio" alt=""/></a></div>
         </div>
     );
 };
