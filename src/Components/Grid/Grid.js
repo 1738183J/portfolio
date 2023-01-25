4ffcb2fe69d24814d08f98557c7958d902b11729
1738183J <1738183@cstj.qc.ca>
2023-01-25 18:21:06 -0500

    link updated

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -10,7 +10,7 @@ const Grid = props => {
         <div className="grid-container">
             <div className='item'><a href="https://adroise.netlify.app/"><img src = {Ardoise} className="ardoise" alt=""/></a></div>
             <div className='item'><a href="https://jacobgodbout3.myportfolio.com/"><img src = {Portfolio} className="portfolio" alt=""/></a></div>
-            <div className='item'><a href="https://oniorigin.web.app/"><img src = {Oni} className="portfolio" alt=""/></a></div>
+            <div className='item'><a href="https://oniorigin.firebaseapp.com/"><img src = {Oni} className="portfolio" alt=""/></a></div>
         </div>
     );
 };
